43762820c95c0dd2372dfc8f4c210cc6699dc223
Rio Chandra <riochandra4@gmail.com>
2022-03-25 14:32:29 +0700

    ref: swagger become 1 file

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -4,18 +4,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AppInitLogger } from './logger/app-init.logger';
 import { RouteParent } from './route-parent';
+import { setupSwagger } from './utils/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const port = process.env.PORT || 3000;
-
-  const config = new DocumentBuilder()
-    .setTitle('Swagger E Commerce Headless')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup(RouteParent.server.swagger, app, document);
-
+  setupSwagger(app);
   app.enableShutdownHooks();
   await app.listen(port);
   AppInitLogger();
